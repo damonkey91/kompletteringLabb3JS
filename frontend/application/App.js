53a6f12c356c1e2d9cf2de4c9727b16a19f57e6a
Tobias Johansson <TobiasJohansson911@gmail.com>
2018-10-02 20:50:43 +0200

    implemented navigation

--- frontend/application/App.js
+++ frontend/application/App.js
@@ -1,61 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, TouchableHighlight, Image } from 'react-native';
-import iconAdd from './assets/add.png';
+import { Home } from './views/Home.js';
+import {createStackNavigator} from 'react-navigation';
+
+const MyRoutes = createStackNavigator({
+  HomeRT: { screen: Home },
+},
+{
+  initialRouteName: "HomeRT"
+});
 
 export default class App extends React.Component {
-
-constructor(props) {
-  super(props)
-  this.state = {
-    quote: " "
-  }
-}
-
-  fetchPositiveQute(){
-      fetch('http://127.0.0.1:3000/one')
-      .then(response => response.json())
-      .then(result => {
-        console.log('Här fetchas från databasen');
-        console.log(result);
-        this.setState({quote: result.quote})
-      })
-  }
-
   render() {
+    {console.log("app");}
     return (
-      <View style={styles.container}>
-        <Text>{this.state.quote}</Text>
-        <Button
-          onPress={() => this.fetchPositiveQute()}
-          title="Get positive quote"
-          color="#841584"
-        />
-      <TouchableHighlight style={styles.imageContainer} onPress={() => {console.log("frefrefrefrefrefrefrefrefre");}}>
-        <Image
-
-          source={require('./assets/add.png')}
-          style={styles.iconButton}
-          />
-      </TouchableHighlight>
-      </View>
+      <MyRoutes/>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#737373',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  imageContainer: {
-    margin: '5%',
-    borderRadius: 360,
-    backgroundColor: '#41727E',
-    elevation: 5
-  },
-  iconButton: {
-    borderRadius: 360,
-  }
-});
---
+++ frontend/application/views/Home.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button, TouchableHighlight, Image } from 'react-native';
+import {createStackNavigator} from 'react-navigation';
+
+export class Home extends React.Component {
+  static navigationOptions = {
+    header: null
+  }
+constructor(props) {
+  super(props)
+  this.state = {
+    quote: " "
+  }
+}
+
+fetchPositiveQute(){
+    fetch('http://127.0.0.1:3000/one')
+    .then(response => response.json())
+    .then(result => {
+      console.log('Här fetchas från databasen');
+      console.log(result);
+      this.setState({quote: result.quote})
+    })
+}
+
+  render() {
+    const {navigate} = this.props.navigation;
+    {console.log("home");}
+    return (
+      <View style={styles.container}>
+        <Text>{this.state.quote}</Text>
+        <Button
+          onPress={() => this.fetchPositiveQute()}
+          title="Get positive quote"
+          color="#841584"
+        />
+      <TouchableHighlight style={styles.imageContainer} onPress={() => {console.log("frefrefrefrefrefrefrefrefre");}}>
+        <Image
+
+          source={require('../assets/add.png')}
+          style={styles.iconButton}
+          />
+      </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#737373',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imageContainer: {
+    margin: '5%',
+    borderRadius: 360,
+    backgroundColor: '#41727E',
+    elevation: 5
+  },
+  iconButton: {
+    borderRadius: 360,
+  }
+});
